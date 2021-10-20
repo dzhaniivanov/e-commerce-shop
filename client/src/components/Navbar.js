4e3e1552ccd82e84e7160d32f4214c6c22ce1f78
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-20 15:00:46 +0300

    setting up client side

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`;
+
+const Navbar = () => {
+    return (
+        <Container>
+            navbar
+        </Container>
+    )
+}
+
+export default Navbar
