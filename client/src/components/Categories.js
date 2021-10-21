7e6acc2d6739db3a0501c0df624a71f9debb774e
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-21 15:46:00 +0300

    products component added

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -5,13 +5,14 @@ import CategoryItem from "./CategoryItem";
 const Container = styled.div`
     display: flex;
     padding: 20px;
+    justify-content:space-between;
 `;
 
 const Categories = () => {
     return (
         <Container>
-            {categories.map((item)=>(
-                <CategoryItem item={item}/>
+            {categories.map((item) => (
+                <CategoryItem item={item} key={item.id} />
             ))}
         </Container>
     )
---
+++ client/src/components/Product.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { ShoppingCartOutlined, SearchOutlined, FavoriteBorderOutlined } from "@material-ui/icons";
+
+const Container = styled.div`
+    flex:1;
+    margin: 5px;
+`;
+
+const Circle = styled.div``;
+const Image = styled.img``;
+const Info = styled.div``;
+const Icon = styled.div``;
+
+
+const Product = ({ item }) => {
+    return (
+        <Container>
+            <Circle />
+            <Image src={item.img} />
+            <Info>
+                <Icon>
+                    <ShoppingCartOutlined />
+                </Icon>
+                <Icon>
+                    <SearchOutlined />
+                </Icon>
+                <Icon>
+                    <FavoriteBorderOutlined />
+                </Icon>
+            </Info>
+        </Container>
+    )
+}
+
+export default Product
