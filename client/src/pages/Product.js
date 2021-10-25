b7f1b4faf26e45d26a81fe44f497297ac2bf4494
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-25 15:43:24 +0300

    styling single product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -3,16 +3,113 @@ import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
+import { Add, Remove } from "@material-ui/icons";
 
 
 const Container = styled.div``;
-const Wrapper = styled.div``;
-const ImgContainer = styled.div``;
-const Image = styled.img``;
-const InfoContainer = styled.div``;
-const Title = styled.h1``;
-const Desc = styled.p``;
-const Price = styled.span``;
+
+const Wrapper = styled.div`
+    padding:50px;
+    display: flex;
+`;
+
+const ImgContainer = styled.div`
+    flex:1;
+`;
+
+const Image = styled.img`
+    width:100%;
+    height: 90vh;
+    object-fit:cover;
+`;
+
+const InfoContainer = styled.div`
+    flex:1;
+    padding:0px 50px;
+`;
+
+const Title = styled.h1`
+    font-weight: 200;
+`;
+
+const Desc = styled.p`
+    margin:20px 0px;
+`;
+const Price = styled.span`
+    font-weight:100;
+    font-size:40px;
+`;
+
+const FilterContainer = styled.div`
+    width:50%;
+    margin:30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const FilterTitle = styled.div`
+    font-size: 20px;
+    font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius:50%;
+    background-color: ${props => props.color};
+    margin:0px 5px;
+    cursor:pointer;
+`;
+
+
+const FilterSize = styled.select`
+    margin-left:10px;
+    padding:5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+    width:50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    font-weight: 700;
+`;
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius:10px;
+    border:1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin:0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border:2px solid teal;
+    background-color: white;
+    cursor:pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
+
+
 
 
 
@@ -30,6 +127,32 @@ const Products = () => {
                     <Title>Bomber Jacket</Title>
                     <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed earum praesentium autem adipisci, excepturi officia accusamus odio vel hic aliquam.</Desc>
                     <Price>$20</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Color</FilterTitle>
+                            <FilterColor color="black" />
+                            <FilterColor color="darkblue" />
+                            <FilterColor color="gray" />
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Size</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>XS</FilterSizeOption>
+                                <FilterSizeOption>S</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>L</FilterSizeOption>
+                                <FilterSizeOption>XL</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove />
+                            <Amount>1</Amount>
+                            <Add />
+                        </AmountContainer>
+                        <Button>ADD TO CART</Button>
+                    </AddContainer>
                 </InfoContainer>
             </Wrapper>
             <Newsletter />
