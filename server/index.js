bc1c62d6ad5895f287d2efeb03e8c4765d47c0ee
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-27 10:36:26 +0300

    added jsonwebtoken

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ app.use("/api/auth", authRoute);
 
 
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Backend server is running!")
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}!`)
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //register
 router.post("/register", async (req, res) => {
@@ -23,13 +24,28 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username });
-        !user && res.status(401).json("Wrong credentials!")
+        if (!user) {
+            return res.status(401).json("Wrong credentials!");
+        }
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SECRET);
-        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
-        password !== req.body.password && res.status(401).json("Wrong credentials!")
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-        res.status(200).json(user);
+        if (originalPassword !== req.body.password) {
+            return res.status(401).json("Wrong credentials!");
+        };
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        }, process.env.JWT_SECRET,
+            { expiresIn: "3d" }
+        )
+
+        const { password, ...others } = user._doc;
+
+
+        res.status(200).json({ ...others, accessToken });
     } catch (error) {
         res.status(500).json(error);
     }
