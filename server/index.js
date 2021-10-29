7c30ce2889e9252b513eec699a81236918f3d0f7
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-29 16:20:37 +0300

    stripe integration

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+
 const cors = require("cors");
 
 
@@ -30,6 +32,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout",stripeRoute)
 
 
 
