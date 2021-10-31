5941bbe017cc31cf8deba18e222d340b70d23094
dzhani ivanov <djani.ivanov94@gmail.com>
2021-10-31 13:58:47 +0200

    added topbar for admin panel

---
+++ admin/src/components/Topbar/Topbar.js
@@ -0,0 +1,31 @@
+import "./Topbar.css"
+import { NotificationsNone,Language,Settings } from "@material-ui/icons";
+
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
